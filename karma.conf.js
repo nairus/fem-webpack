9db334fd5318bec68aab551aa4505db934b678d4
Nicolas Surian <nicolas.surian@gmail.com>
2018-04-05 13:58:18 +0200

    2.0 + 2.1 - init karma et add assertions

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = config => {
+    config.set({
+        basePath: '',
+        frameworks: ['mocha', 'chai'],
+        files: [
+            'src/**/*.test.js',
+        ],
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['Chrome'],
+        singleRun: true,
+        concurrency: Infinity,
+    })
+}
---
+++ src/controller.test.js
@@ -0,0 +1,8 @@
+describe('test', () => {
+    it('works', () => {
+        expect(true).to.be.true
+    })
+    it('works again', () => {
+        expect('hi').to.equal('hi')
+    })
+})
